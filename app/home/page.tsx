9da985631cf60e740e51cb405f2f265dff102027
HristofarDimitrov <67313156+HristofarDimitrov@users.noreply.github.com>
2024-09-27 12:06:20 +0300

    test

---
+++ app/home/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>KURS BNB GOLD WOTLK TO WAR WITHIN</h1>
+      <h2>1000g(wotlk blizzard) = 33333g(war within blizzard)</h2>
+    </div>
+  );
+};
+
+export default Home;
